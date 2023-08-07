dc919a2854a78bb7a7821d48cd1798200bd946c4
nhsuh <nsuh813@gmail.com>
2023-08-07 17:17:14 -0500

    fix tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-      css: ['@/assets/css/main.css'],
+      css: ['~/assets/css/main.css'],
       postcss: {
         plugins: {
           tailwindcss: {},
           autoprefixer: {},
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
         },
       },
         modules: ['@nuxtjs/google-fonts'],
